690722c070f5e71dfdf718304daa4de085a4a528
ARPIT KHANDELWAL <arpit.khandelwal.1984@gmail.com>
2016-05-09 12:27:36 +0530

    updated app details

--- fb.js
+++ fb.js
@@ -1,6 +1,6 @@
 // facebook app settings
 module.exports = {
-	'appID' : '874289312699626',
-	'appSecret' : '8c3248decb19d7eeac398f992b5da8a7',
+	'appID' : 'your app id',
+	'appSecret' : 'your app secret',
 	'callbackUrl' : 'http://localhost:3000/login/facebook/callback'
 }

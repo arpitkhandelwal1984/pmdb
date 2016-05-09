252e182b0296c988b3d0a03b8305c865a3c942f9
ARPIT KHANDELWAL <arpit.khandelwal.1984@gmail.com>
2016-05-09 12:15:56 +0530

    PMDB First commit

---
+++ fb.js
@@ -0,0 +1,6 @@
+// facebook app settings
+module.exports = {
+	'appID' : '874289312699626',
+	'appSecret' : '8c3248decb19d7eeac398f992b5da8a7',
+	'callbackUrl' : 'http://localhost:3000/login/facebook/callback'
+}
